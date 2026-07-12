91c0fb01e46fcc0129afeb38f43f69bf4b90535c
Ishini U Kohomban Arachchi <iukarachchi@students.nsbm.ac.lk>
2026-07-12 19:24:33 +0530

    Add Terraform Lab 001 - Create AWS EC2 Key Pair
    Added a Terraform configuration to create an AWS EC2 Key Pair.

---
+++ Terraform/Lab001-Create-AWS-KeyPair/main.tf
@@ -0,0 +1,24 @@
+# Terraform Lab 001 - Create AWS Key Pair
+
+## Objective
+Create an AWS EC2 Key Pair named `datacenter-kp` using Terraform.
+
+## Resources
+- tls_private_key
+- aws_key_pair
+- local_file
+
+## Commands
+
+```bash
+terraform init
+terraform plan
+terraform apply
+```
+
+## Skills Learned
+- Terraform resources
+- AWS Provider
+- TLS Provider
+- Local Provider
+- Creating EC2 Key Pairs
